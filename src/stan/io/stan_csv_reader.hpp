fa3a6fa2d0ebfefdd0e046ccdae921aa539ab007
Stan Jenkins <mc.stanislaw@gmail.com>
2024-11-27 17:49:02 -0500

    [Jenkins] auto-formatting by clang-format version 10.0.0-4ubuntu1

--- src/stan/io/stan_csv_reader.hpp
+++ src/stan/io/stan_csv_reader.hpp
@@ -334,12 +334,12 @@ class stan_csv_reader {
         for (int col = 0; col < cols; col++) {
           std::getline(ls, line, ',');
           boost::trim(line);
-	  try {
-	    auto raw = std::stold(line);
-	    samples(row, col) = static_cast<double>(raw);
-	  } catch (const std::out_of_range &e) {
-	    samples(row, col) = std::numeric_limits<double>::quiet_NaN();
-	  }
+          try {
+            auto raw = std::stold(line);
+            samples(row, col) = static_cast<double>(raw);
+          } catch (const std::out_of_range& e) {
+            samples(row, col) = std::numeric_limits<double>::quiet_NaN();
+          }
         }
       }
     }
